be6f1b5b2b7c76e55643c2b3e8a8749882ad9b82
Franco Traverso <franco-t20@outlook.com>
2022-08-08 19:35:04 -0300

    Render dynamic mocked data instead of hardcoded

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -31,38 +31,34 @@ const theme = createTheme({
   },
 });
 
-const TourCard = () => {
+const TourCard = ({ tour }) => {
   return (
     <Grid item xs={3}>
       <ThemeProvider theme={theme}>
         <Paper elevation={3}>
-          <img
-            src="https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg"
-            alt="niagara falls"
-            className="img"
-          />
+          <img src={tour.image} alt="niagara falls" className="img" />
           <Box paddingX={1}>
             <Typography variant="subtitle1" component="h2">
-              Immerse into the Falls
+              {tour.name}
             </Typography>
             <Box sx={{ display: "flex", alignItems: "center" }}>
               <AccessTime sx={{ width: 12.5 }} />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                5 hours
+                {tour.duration} hours
               </Typography>
             </Box>
             <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-              <Rating name="read-only" value={4.5} precision={0.5} size="small" readOnly />
+              <Rating name="read-only" value={tour.rating} precision={0.5} size="small" readOnly />
               <Typography variant="body2" component="p" marginLeft={0.5}>
-                4.5
+                {tour.rating}
               </Typography>
               <Typography variant="body3" component="p" marginLeft={1.5}>
-                (655 reviews)
+                {tour.numberOfReviews}
               </Typography>
             </Box>
             <Box>
               <Typography variant="h6" component="h3" marginTop={0}>
-                From C $147
+                From C ${tour.price}
               </Typography>
             </Box>
           </Box>
