c8ca9c44fdd0c19e80f7f03b17b64cf899f17b4a
Franco Traverso <franco-t20@outlook.com>
2022-08-07 22:59:43 -0300

    Add custom theme, applied to all tour cards

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,42 +4,70 @@ import Grid from "@mui/material/Grid";
 import Rating from "@mui/material/Rating";
 import Typography from "@mui/material/Typography";
 import { AccessTime } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg"
-          alt="niagara falls"
-          className="img"
-        />
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <AccessTime sx={{ width: 12.5 }} />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://cdn.pixabay.com/photo/2016/08/13/07/20/niagara-1590345_960_720.jpg"
+            alt="niagara falls"
+            className="img"
+          />
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
             </Typography>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <AccessTime sx={{ width: 12.5 }} />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
+            <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
+              <Rating name="read-only" value={4.5} precision={0.5} size="small" readOnly />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (655 reviews)
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center" }} marginTop={3}>
-            <Rating name="read-only" value={4.5} precision={0.5} size="small" readOnly />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (655 reviews)
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
