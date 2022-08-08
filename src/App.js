158e8c9be21bcdafc44b84e499dd3bdf5d442c4c
Franco Traverso <franco-t20@outlook.com>
2022-08-08 19:19:52 -0300

    Add top city name tours titles

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import TourCard from "./components/TourCard";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import SearchAppBar from "./components/Appbar";
+import cities from "./data.json";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
+        {cities.map((city) => (
+          <Typography variant="h4" component="h2" marginTop={5} marginBottom={3}>
+            Top {city.name} Tours
+          </Typography>
+        ))}
+        {/* <Grid container spacing={5}>
           <TourCard />
           <TourCard />
           <TourCard />
           <TourCard />
-        </Grid>
+        </Grid> */}
       </Container>
     </div>
   );
