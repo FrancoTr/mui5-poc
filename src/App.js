feb8ba1df0984f686674aed205b0bc6331b6967e
Franco Traverso <franco-t20@outlook.com>
2022-08-14 19:39:47 -0300

    Move navbar to root component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
+import { AppBar } from "@mui/material";
+import SearchAppBar from "./components/AppBar";
 
 function App() {
   return (
     <BrowserRouter>
+      <SearchAppBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
