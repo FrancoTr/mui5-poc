dacff5fd58504acd7fe9fc2c9f32da3ada2902dd
Franco Traverso <franco-t20@outlook.com>
2022-08-16 19:10:06 -0300

    Add title to tour page

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,7 +1,13 @@
+import { Container } from "@mui/material/Container";
+import { Typography } from "@mui/material";
 import React from "react";
 
 function Tour() {
-  return <div>Tour</div>;
+  return (
+    <Container sx={{ width: 900 }}>
+      <Typography>Explore the World</Typography>
+    </Container>
+  );
 }
 
 export default Tour;
