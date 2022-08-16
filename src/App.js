1b783b8880f486e65e4de1ae39cfbea10ade4b4d
Franco Traverso <franco-t20@outlook.com>
2022-08-16 01:26:38 -0300

    Remove unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
-import { AppBar } from "@mui/material";
 import SearchAppBar from "./components/AppBar";
 import Tour from "./pages/Tour";
 
