f287e1c04b4198db9b0d559f84c84432c9e5e029
Franco Traverso <franco-t20@outlook.com>
2022-08-23 22:13:39 -0300

    Add date picker calendar feature to tour page modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import StaticDateRangePickerDemo from "./DatePicker";
 
 const style = {
   position: "absolute",
@@ -31,12 +31,7 @@ export default function BasicModal() {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <StaticDateRangePickerDemo />
         </Box>
       </Modal>
     </div>
